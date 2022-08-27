17b31c29e81d557c9af5b37081227a0d3e677ef2
Ilyas <ilyasaydarkhanov@gmail.com>
2022-08-28 02:20:53 +0600

    Get rid of Link object and return link in robot find_link method using soup

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import requests
+from bs4 import BeautifulSoup
 
 
 class Page:
@@ -11,21 +12,16 @@ class Page:
         self.content = response.text
 
 
-class Link:
-    text = 'Заңды тұлғаларға'
-
-    def __str__(self):
-        return '<a'
-
-
 class Robot:
     current_page = None
 
     def open_page(self, url):
         self.current_page = Page(url)
 
-    def find_link(self, link_text):
-        return Link()
+    def find_link(self, text):
+        soup = BeautifulSoup(self.current_page.content)
+        link = soup.find('a', text=text)
+        return link
 
 
 class RobotBrowsePagesTest(TestCase):
@@ -33,17 +29,14 @@ class RobotBrowsePagesTest(TestCase):
     def setUp(self):
         self.url = 'https://nursultan.kgd.gov.kz/'
         self.robot = Robot()
-
-    def test_can_open_start_page(self):
         self.robot.open_page(self.url)
 
+    def test_can_open_start_page(self):
         self.assertEqual(self.robot.current_page.status, 200)
         self.assertEqual(self.robot.current_page.url, self.url)
 
     def test_can_get_page_content(self):
         page_title = "Мемлекеттік Кірістер Департаменті"
-
-        self.robot.open_page(self.url)
 
         self.assertIn(page_title, self.robot.current_page.content)
 
