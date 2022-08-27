b1280f6ba671f3460c5e31c644bae11067609c82
Ilyas <ilyasaydarkhanov@gmail.com>
2022-08-28 01:57:20 +0600

    Change start to open page method

--- tests.py
+++ tests.py
@@ -14,29 +14,25 @@ class Page:
 class Robot:
     current_page = None
 
-    def __init__(self, start_url):
-        self.start_url = start_url
-
-    def start(self):
-        self.current_page = Page(self.start_url)
+    def open_page(self, url):
+        self.current_page = Page(url)
 
 
 class RobotBrowsePagesTest(TestCase):
 
+    def setUp(self):
+        self.url = 'https://nursultan.kgd.gov.kz/'
+        self.robot = Robot()
+
     def test_can_open_start_page(self):
-        given_url = 'https://nursultan.kgd.gov.kz/'
+        self.robot.open_page(self.url)
 
-        robot = Robot(start_url=given_url)
-        robot.start()
-
-        self.assertEqual(robot.current_page.status, 200)
-        self.assertEqual(robot.current_page.url, given_url)
+        self.assertEqual(self.robot.current_page.status, 200)
+        self.assertEqual(self.robot.current_page.url, self.url)
 
     def test_can_get_page_content(self):
-        given_url = 'https://nursultan.kgd.gov.kz/'
         page_title = "Мемлекеттік Кірістер Департаменті"
 
-        robot = Robot(start_url=given_url)
-        robot.start()
+        self.robot.open_page(self.url)
 
-        self.assertIn(page_title, robot.current_page.content)
+        self.assertIn(page_title, self.robot.current_page.content)
