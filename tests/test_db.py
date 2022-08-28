00ec5564f234049736cfd8a8462d771ce0d203c7
Ilyas <ilyasaydarkhanov@gmail.com>
2022-08-28 19:00:50 +0600

    Create table and add some columns

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,30 @@
 from unittest import TestCase
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from pathlib import Path
+
+
+class DBManager:
+
+    def __init__(self):
+        self.engine = create_engine('sqlite:///../sqlite3.db')
+        self.engine.connect()
+
+    def create_table(self, param):
+        meta = MetaData()
+        Table(
+            'companies', meta,
+            Column('id', Integer, primary_key=True),
+            Column('iin', Integer),
+            Column('name', String),
+            Column('registration_number', String),
+        )
+        meta.create_all(self.engine)
 
 
 class DBManagerTest(TestCase):
 
-    def test_create_db(self):
-        my_file = Path("../sqlite3.db")
-        engine = create_engine('sqlite:///../sqlite3.db')
-        engine.connect()
+    def test_db_can_create_table(self):
+        db = DBManager()
+        db.create_table('companies')
 
-        self.assertTrue(my_file.is_file())
+        self.assertTrue(db.engine.has_table('companies'))
