043bf791a8e9def00ddf519c8b9191f264dda05a
Ilyas <ilyasaydarkhanov@gmail.com>
2022-08-29 11:21:52 +0600

    Gets all rows with columns

---
+++ tests/test_excel_reader.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+import openpyxl
+
+from examples import EXAMPLE_COMPANY
+
+
+class Reader:
+    def __init__(self, path):
+        self.path = path
+
+    def sheet(self):
+        workbook = openpyxl.load_workbook(self.path)
+        return workbook.active
+
+    def all(self):
+        sheet = self.sheet()
+        rows = []
+        for row in sheet.iter_rows(min_row=6, min_col=1,
+                                   max_row=sheet.max_row,
+                                   max_col=sheet.max_column):
+            rows.append(row)
+        return rows
+
+
+class ExcelReaderTest(TestCase):
+
+    def setUp(self) -> None:
+        path = "../list.xlsx"
+        self.reader = Reader(path)
+
+    def test_can_read_first_row(self):
+        expected_row = EXAMPLE_COMPANY
+
+        sheet = self.reader.sheet()
+
+        self.assertEqual(expected_row[0], list(sheet.rows)[5][1].value)
+
+    def test_can_read_all_rows(self):
+        rows = self.reader.all()
+
+        self.assertEqual(712, len(rows))
+        self.assertEqual(14, len(rows[0]))
+        self.assertEqual(14, len(rows[-1]))
