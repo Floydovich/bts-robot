0088776c286f9d1269f0ae59dbc58902f46970e3
Ilyas <ilyasaydarkhanov@gmail.com>
2022-08-29 11:57:25 +0600

    Add some user inputs in main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from robot import Robot
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    robot = Robot('https://nursultan.kgd.gov.kz/', 'ru')
+
+    section = input("Введите название нужного раздела сайта: ")
+
+    page = robot.find_link(section)
+    print(page)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
